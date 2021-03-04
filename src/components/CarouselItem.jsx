e488dc02d786717427f6219506b20b6ce960dbbf
Sebastian Castillo <sebastian@MacBook-Pro-de-Sebastian.local>
2021-03-03 23:44:53 -0300

    mejoras visuales

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -14,7 +14,6 @@ const CarouselItem = ({cover, title, year, contentRating, duration}) => (
       </div>
       <p className='carousel-item__details--title'>{title}</p>
       <p className='carousel-item__details--subtitle'>{`${year} ${contentRating} ${duration} `}   minutos</p>
-       {`${year} ${contentRating} ${duration} `}    
     </div>
   </div>
 );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -11,6 +11,7 @@ import useInitialState from '../hooks/useInitialState';
 const API = 'http://localhost:3000/initalState';
 const App = () => {
   const initialState = useInitialState(API);
+  const carouselRef = carouselUseRef();
   return initialState.length === 0 ? <h1>Sin resultados...</h1> : (
     <div className='App'>
       <Header />
@@ -25,7 +26,7 @@ const App = () => {
 
       {initialState.trends.length > 0 && (
         <Categories title='Tendencias'>
-          <Carousel>
+          <Carousel >
             {initialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
 
           </Carousel>
